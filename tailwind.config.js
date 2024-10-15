6bc9be68c9716dec042857a220a4bdb06513e02e
NithyadeviChandran <nithyadevi.ch@gmail.com>
2024-10-15 15:59:47 +0200

    Created new project Pflegepfad and installed the tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#01649D',
+      },
+    },
+  },
+  plugins: [
+    tailwindcss,
+    autoprefixer
+  ],
+};
+
