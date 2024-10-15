1d9180f298a1d1fcc8561e85923b2b082686468f
NithyadeviChandran <nithyadevi.ch@gmail.com>
2024-10-15 17:39:55 +0200

    Added the Pflegepfad image as background in the Startpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header'
 import Footer from './Footer'
 import {Routes, Route} from 'react-router-dom'
+import Startpage from './Startpage';
 
 
 
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
     <Header/>
+    <Routes>
+      <Route path="/" element={<Startpage />}/>
+    </Routes>
     <Footer/>
     </>
   )
--- src/Startpage.jsx
+++ src/Startpage.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import Frontpage from './assets/Frontpage.png'
+import Path from './assets/Path.svg'
 
 function Startpage() {
   return (
-    <div>Startpage</div>
-  )
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-grow mt-12">
+        <div className="container mx-auto px-4 py-8 md:py-16">
+          <h2 className="text-xl sm:text-2xl md:text-3xl text-blue-900 font-bold mb-4">
+            Ihr Weg durch die Pflege - Schritt für Schritt
+          </h2>
+          <div className="flex justify-between mt-2 mb-2 border rounded border-blue-900 p-2 bg-blue-100 w-full max-w-md md:max-w-2xl">
+            <p className="whitespace-normal text-blue-900 font-sans">
+              Die Reise geht los! Hier sehen Sie, wie ein beispielhafter Verlauf
+              Ihrer Pflegetätigkeit als Angehöriger aussehen könnte:
+            </p>
+            <img
+              className="h-24 fill-blue-900 bg-blue-100"
+              src={Frontpage}
+              alt="Description"
+            />
+          </div>
+          
+          <div 
+          className="relative w-full max-w-4xl mx-auto z-0"
+            style={{
+            minHeight: "700px",
+            backgroundImage: `url(${Path})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "contain",
+        }}
+        >
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default Startpage
+export default Startpage;
