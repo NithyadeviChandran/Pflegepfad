f582221b65bc0e7cc6ecb6d6727e65654d55443b
NithyadeviChandran <nithyadevi.ch@gmail.com>
2024-10-28 20:44:22 +0100

    Swipeable added for the slides

--- src/Slidepage.jsx
+++ src/Slidepage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useSlideData } from "./SlideContext";
 import { HiOutlineShare } from "react-icons/hi";
+import { useSwipeable } from "react-swipeable"; 
 import arrow from "./assets/arrow.png";
 import {
   FaFacebook,
@@ -53,6 +54,13 @@ function Slidepage() {
     };
   }, []);
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => nextSlide(),
+    onSwipedRight: () => prevSlide(),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true, // Allows swipe functionality in browser for testing
+  });
+
   return (
     <div className="p-4 mt-16 md:p-16" style={{ position: "relative" }}>
       <Link
@@ -68,7 +76,7 @@ function Slidepage() {
       </h2>
 
       {slides && slides.length > 0 ? (
-        <div className="relative max-w-3xl mx-auto">
+        <div className="relative max-w-3xl mx-auto" {...handlers} >
           {currentSlide > 0 && (
             <button
               className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
